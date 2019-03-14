92261139892f7fe277378e4a4ef57d877442a93c
Ritu Gupta <ritugupta@ritugupta-macbookpro.roam.corp.google.com>
2019-03-14 12:30:27 +0530

    Updates

---
+++ precache-manifest.3a953a4756e8973b59a073f636cfc8a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c46cf9ee4bf95027128",
+    "url": "/react-meme-generator/static/js/runtime~main.b08da8a3.js"
+  },
+  {
+    "revision": "893f08ae779c15e8d080",
+    "url": "/react-meme-generator/static/js/main.4be994c5.chunk.js"
+  },
+  {
+    "revision": "73080c8c5313bf1fb476",
+    "url": "/react-meme-generator/static/js/2.d519b009.chunk.js"
+  },
+  {
+    "revision": "893f08ae779c15e8d080",
+    "url": "/react-meme-generator/static/css/main.f51364ba.chunk.css"
+  },
+  {
+    "revision": "628d6b21034d9e68d9ff754de061711d",
+    "url": "/react-meme-generator/index.html"
+  }
+];
